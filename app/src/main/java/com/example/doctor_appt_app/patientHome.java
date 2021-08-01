d581b6e76d94c37191421c8d37998c0f948a13a8
PitcairnHotdog <pitcairnzhang@outlook.com>
2021-07-31 23:11:51 -0400

    Changed patient_home; Added XML for doctor selection, booking and viewing existing appointment

--- app/src/main/java/com/example/doctor_appt_app/patientHome.java
+++ app/src/main/java/com/example/doctor_appt_app/patientHome.java
@@ -17,13 +17,7 @@ public class patientHome extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_home);
-        lv = findViewById(R.id.listview1);
 
-        items = new ArrayList<String>();
-
-        items.add("Thursday, July 12th, 2022");
-        items.add("Friday, July 13th, 2022");
-        items.add("Saturday, July 14th, 2022");
 
         ArrayAdapter<String> adapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, items);
         lv.setAdapter(adapter);
